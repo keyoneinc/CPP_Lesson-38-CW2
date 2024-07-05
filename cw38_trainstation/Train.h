139873e1de08a3ea530a933f06fcd766d6a0716e
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-05 19:32:43 +0100

    included Time_ library, and added Train class, added some code in Train.h

---
+++ cw38_trainstation/Train.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Time_.h"
+using namespace std;
+
+class Train {
+	string number;
+	Time_ depature;
+	string direction;
+
+public:
+	Train();
+	Train(string number, Time_ depature, string direction);
+
+	void setNumber(string number);
+	void setDepature(Time_ depature);
+	void setDirection(string direction);
+
+	string getNumber()const;
+	Time_ getDepature()const;
+	string getDirection()const;
+
+	void showInfo()const;
+
+};
+
